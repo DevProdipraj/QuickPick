f069e1b954ed5b22bbe28d3d3d5c98c4c7c225bb
Prodipraj <115894214+DevProdipraj@users.noreply.github.com>
2024-11-16 11:59:28 +0600

    Update HeroSlider.jsx

--- Client/src/components/HeroSlider.jsx
+++ Client/src/components/HeroSlider.jsx
@@ -2,10 +2,10 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import SliderImg1 from "../assets/HeroSliderImg/Img2.webp";
-// import SliderImg2 from "../assets/HeroSliderImg/Img1.webp";
-// import SliderImg3 from "../assets/HeroSliderImg/img3.webp";
-// import SliderImg4 from "../assets/HeroSliderImg/Img4.webp";
+import Img1 from "../assets/HeroSliderImg/Img2.webp";
+import Img2 from "../assets/HeroSliderImg/Img1.webp";
+import Img3 from "../assets/HeroSliderImg/img3.webp";
+import Img4 from "../assets/HeroSliderImg/Img4.webp";
 
 function HeroSlider() {
   const settings = {
@@ -21,28 +21,28 @@ function HeroSlider() {
         <div>
           <img
             className=" h-[30vh] md:h-[70vh]   w-full"
-            src={SliderImg1}
+            src={Img2}
             alt=""
           />
         </div>
         <div>
           <img
             className=" h-[30vh] md:h-[70vh]   w-full border-0  outline-0"
-            src={SliderImg1}
+            src={Img1}
             alt=""
           />
         </div>
         <div>
           <img
             className=" h-[30vh] md:h-[70vh]  w-full border-0  outline-0"
-            src={SliderImg1}
+            src={Img3}
             alt=""
           />
         </div>
         <div>
           <img
             className=" h-[30vh] md:h-[70vh]   w-full border-0  outline-0"
-            src={SliderImg1}
+            src={Img4}
             alt=""
           />
         </div>
